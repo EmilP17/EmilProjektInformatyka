9519fb575893408170371b5a550cad4bedef0813
EmilP17 <emilpokolenko025@gmail.com>
2026-06-10 12:34:13 +0200

    losowe statki\

--- gra.cpp
+++ gra.cpp
@@ -1,4 +1,4 @@
-#include <iostream><cmath>
+#include <iostream> <cmath>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
@@ -6,31 +6,57 @@ int main(){
 
 
         cout << "                                                      GRA STATKI" << endl << endl;
-        srand(time (0));
-        int statekX= rand()%10;
-        int statekY= rand()%10;
-        char plansza[10][10];
+ srand(time(0));
 
+    char plansza[10][10];
 
-            for(int i=0; i<10; i++){
-                for(int j=0; j<10; j++){
-                     plansza[i][j] ='~';
-                }
-            }
-                for(int i=0; i<10; i++){
-                    for(int j=0; j<10; j++){
-                        cout << plansza[i][j] << " ";
-                    }
-                    cout<< endl;
-                }
+    // Wype³nienie planszy
+    for(int i=0; i<10; i++)
+    {
+        for(int j=0; j<10; j++)
+        {
+            plansza[i][j] = '~';
+            cout << plansza[i][j] << " ";
+        }
+        cout <<endl;
+    }
 
-        int x, y;
+    // Statek 4-polowy
+ int statek4X = rand()%10;
+ int statek4Y = rand()%7;
+
+for(int i=0; i<4; i++)
+{
+    plansza[statek4X][statek4Y+i] = 'X';
+}
+
+// Statek 3-polowy
+ int statek3X = rand()%8;
+ int statek3Y = rand()%10;
+
+for(int i=0; i<3; i++)
+{
+    plansza[statek3X+i][statek3Y] = 'X';
+}
+
+// Statek 2-polowy
+ int statek2X = rand()%10;
+ int statek2Y = rand()%9;
+
+for(int i=0; i<2; i++)
+{
+    plansza[statek2X][statek2Y+i] = 'X';
+}
+
+        int a, b;
 
         cout << "Podaj wspolrzedne: " << endl;
         cout << "Podaj wiersz: ";
-        cin >> x;
+        cin >> a;
         cout << "Podaj kolumne: ";
-        cin >> y;;
-
-
+        cin >> b;
+        if(a>) //dokoncz jesli jest wiecej i mniej niz mozna to ze zle dane
 }
+
+
+
